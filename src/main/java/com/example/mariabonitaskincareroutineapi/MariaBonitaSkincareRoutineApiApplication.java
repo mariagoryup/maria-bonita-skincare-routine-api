8925ee51eaebc2c9ba1a1589b62b7c64ccb3b9a9
mariagoryup <133912905+mariagoryup@users.noreply.github.com>
2023-08-11 13:59:59 +0200

    Initial Commit

---
+++ src/main/java/com/example/mariabonitaskincareroutineapi/MariaBonitaSkincareRoutineApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mariabonitaskincareroutineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariaBonitaSkincareRoutineApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MariaBonitaSkincareRoutineApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mariabonitaskincareroutineapi/MariaBonitaSkincareRoutineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mariabonitaskincareroutineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariaBonitaSkincareRoutineApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
